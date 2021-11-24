ac385f37d299f2af2e6476119ebf9d21e7d17cc2
Leonardo Rocca <lrocca@student.42roma.it>
2021-11-24 17:11:31 +0100

    ex01: de-comment main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,22 +6,21 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 13:01:44 by lrocca            #+#    #+#             */
-/*   Updated: 2021/11/24 02:35:33 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/11/24 16:43:09 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 
 int	main() {
-	// ScavTrap	caio("Caio"), noia("Noia"), jojo(caio), def;
-	ScavTrap	def;
+	ScavTrap	caio("Caio"), noia("Noia"), jojo(caio), def;
 
-	// caio = noia;
+	caio = noia;
 
-	// caio.attack("Ollo");
-	// noia.takeDamage(100);
-	// jojo.beRepaired(42);
-	// caio.guardGate();
+	caio.attack("Ollo");
+	noia.takeDamage(100);
+	jojo.beRepaired(42);
+	caio.guardGate();
 
 	def.guardGate();
 }
