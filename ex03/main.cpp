b994514ba76a889711d9bb15d88bc88932bb2ebf
Leonardo Rocca <lrocca@student.42roma.it>
2021-11-24 16:05:08 +0100

    ex03: de-comment main

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,25 +6,24 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 13:01:44 by lrocca            #+#    #+#             */
-/*   Updated: 2021/11/24 02:22:40 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/11/24 16:00:39 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "DiamondTrap.hpp"
 
 int	main() {
-	// DiamondTrap	caio("Caio"), noia("Noia"), jojo(caio), def;
-	DiamondTrap	def("hey");
+	DiamondTrap	caio("Caio"), noia("Noia"), jojo(caio), def;
 
-	// caio = noia;
+	caio = noia;
 
-	// caio.attack("Ollo");
-	// noia.takeDamage(100);
-	// jojo.beRepaired(42);
+	caio.attack("Ollo");
+	noia.takeDamage(100);
+	jojo.beRepaired(42);
 
-	// jojo = DiamondTrap("JOJO");
-	// jojo.guardGate();
-	// caio.highFivesGuys();
+	jojo = DiamondTrap("JOJO");
+	jojo.guardGate();
+	caio.highFivesGuys();
 
-	// noia.whoAmI();
+	noia.whoAmI();
 }
